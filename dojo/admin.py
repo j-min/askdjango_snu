c9140a84fd6a54466d760af81366cf13474b511a
Jaemin Cho <heythisischo@gmail.com>
2017-04-12 17:43:30 +0900

    fixed untracked files

--- dojo/admin.py
+++ dojo/admin.py
@@ -4,10 +4,17 @@ from dojo.models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'tags', 'content_size', 'lnglat']
-    list_editable = ['title']
+    list_display = ['id', 'title', 'tags', 'content_size', 'status']
+    list_editable = ['title', 'tags']
+    search_field = ['title']
     # list_display_links = ['id', 'title']
-    list_filter = ['tags']
+    list_filter = ['created_at', 'status']
+    actions = ['make_published']
+
+    def make_published(self, request, queryset):
+        total = queryset.update(status='p')
+        self.message_user(request, '{}건을 발행했습니다.'.format(total))
+    make_published.short_decription = '선택된 항목을 발행하기'
 
     def content_size(self, post):
         html = '<span style="color: blue;">{}</span>글자'.format(len(post.content))
--- dojo/models.py
+++ dojo/models.py
@@ -8,18 +8,35 @@ def lnglat_validator(value):
     if not re.match(r'^(\d+\.?\d*),(\d+\.?\d*)$', value):
         raise ValidationError('Invalid LngLat Type')
 
-choices = ['과학', '경제', '정치']
-choices = ((i, choice) for i, choice in enumerate(choices))
-
-
 class Post(models.Model):
+
+    STATUS_CHOICES = [
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdraw')
+    ]
+
+    TAG_CHOICES = [
+        ('science', '과학'),
+        ('economy', '경제'),
+        ('politics', '정치'),
+        ('sports', '스포츠')
+    ]
+
     title = models.CharField(max_length=100, verbose_name='제목')
     content = models.TextField(help_text='Markdown 문법을 써주세요.')
+    status = models.CharField(max_length=1,
+                            choices=STATUS_CHOICES, default='d')
     tags = models.CharField(max_length=100,
                             blank=True,
-                            choices=choices)
+                            choices=TAG_CHOICES,
+                            default='science')
     lnglat = models.CharField(max_length=50,
                               validators=[lnglat_validator],
+                              blank=True,
                               help_text='경도, 위도 포맷으로 입력')
     created_at = models.DateTimeField(default=timezone.now)
     test_field = models.IntegerField(default=10)
+
+    def __str__(self):
+        return self.title
