eca16e23bdd1ead74c7961294539b8a6038ef1ad
Jaemin Cho <heythisischo@gmail.com>
2017-04-19 16:37:37 +0900

    add link from post_list => post_detail

--- dojo/views.py
+++ dojo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse, Http404
 from django.views.generic import DetailView
 from dojo.models import Post
