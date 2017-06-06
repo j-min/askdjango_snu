5a3a164d650a057e01d780ee172c6ec811dd1b68
Jaemin Cho <heythisischo@gmail.com>
2017-06-06 22:21:34 +0900

    Add media file url serving

--- askdjango/urls.py
+++ askdjango/urls.py
@@ -16,15 +16,21 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),# namespace='blog'),
-    url(r'^dojo/', include('dojo.urls')),# namespace='dojo'),
+    # url(r'^dojo/', include('dojo.urls')),# namespace='dojo'),
     url(r'^myapp/', include('myapp.urls')),
     url(r'^image/', include('image.urls')),
     url(r'^slackbot/', include('slackbot.urls')),
+    url(r'^accounts/', include('accounts.urls')),
+    url(r'^weblog/', include('dojo.urls', namespace='dojo')),
+    url(r'^shop/', include('shop.urls', namespace='shop')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
